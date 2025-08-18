57bfe630f3aff43dee738f20eabb6d19877eb973
toppy368 <toppy36849@gmail.com>
2025-08-19 00:04:46 +0800

    Artisan 指令建立Model、Migration、Factory、Seeder、 Resource、Test

---
+++ app/Models/Models/ShortUrl.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ShortUrl extends Model
+{
+    /** @use HasFactory<\Database\Factories\Models\ShortUrlFactory> */
+    use HasFactory;
+}
