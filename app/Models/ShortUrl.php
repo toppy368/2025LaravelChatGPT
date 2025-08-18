b16b5ff12240ca73c455edaf0e6e0e0ad848838d
toppy368 <toppy36849@gmail.com>
2025-08-19 01:40:12 +0800

    刪除所有註解

--- app/Models/ShortUrl.php
+++ app/Models/ShortUrl.php
@@ -6,12 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class ShortUrl extends Model
 {
-    // 在 fillable 變數中, 'original_url' 可填寫原址網址, 而 'short_code' 可填寫短網址
     protected $fillable = ['original_url', 'short_code'];
 
     public static function generateCode(): string
     {
-        // substr 可限制 API 欄位 （'original_url'及'short_code'）可輸入範圍，這行已限制只能輸入數字0~9級大小寫英文字母
         return substr(str_shuffle('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'), 0, 10);
     }
 }
