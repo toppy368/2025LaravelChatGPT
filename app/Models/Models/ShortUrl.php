e476d6c8434348fd846f8b9752d1cfc4f2a20641
toppy368 <toppy36849@gmail.com>
2025-08-19 00:47:47 +0800

    貼上程式碼

--- app/Models/Models/ShortUrl.php
+++ app/Models/Models/ShortUrl.php
@@ -1,12 +1,15 @@
 <?php
 
-namespace App\Models\Models;
+namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ShortUrl extends Model
 {
-    /** @use HasFactory<\Database\Factories\Models\ShortUrlFactory> */
-    use HasFactory;
+    protected $fillable = ['original_url', 'short_code'];
+
+    public static function generateCode(): string
+    {
+        return substr(str_shuffle('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'), 0, 10);
+    }
 }
