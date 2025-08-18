9952962edf6b7b087dc3ad723ba7a4d62e23d348
toppy368 <toppy36849@gmail.com>
2025-08-19 02:08:30 +0800

    將 short_code 的欄位限制從原本規格書規定的 10 放寬成 20

--- database/migrations/2025_08_18_154053_create_short_urls_table.php
+++ database/migrations/2025_08_18_154053_create_short_urls_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('short_urls', function (Blueprint $table) {
             $table->id();
             $table->text('original_url');
+            $table->string('short_code',20)->unique();
             $table->timestamps();
         });
     }
