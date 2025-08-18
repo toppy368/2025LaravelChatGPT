3b728aef7fbf020168a5cc58798eea600af6f36d
toppy368 <toppy36849@gmail.com>
2025-08-19 01:32:06 +0800

    刪除放錯檔案的註解

--- database/factories/ShortUrlFactory.php
+++ database/factories/ShortUrlFactory.php
@@ -6,7 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class ShortUrl extends Model
 {
-    // 在 fillable 變數中, 'original_url' 可填寫原址網址, 而 'short_code' 可填寫短網址
     protected $fillable = ['original_url', 'short_code'];
 
     public static function generateCode(): string
