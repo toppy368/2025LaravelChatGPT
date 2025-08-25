ff3065ab08e885e9f8ebf2f2b8e8ec219cde280d
toppy368 <toppy36849@gmail.com>
2025-08-26 01:05:08 +0800

    更改 Array 中需要的欄位

--- app/Http/Resources/ShortUrlResource.php
+++ app/Http/Resources/ShortUrlResource.php
@@ -14,6 +14,12 @@ class ShortUrlResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => (string) $this->id,
+            'short_code' => $this->short_code,
+            'original_url' => $this->original_url,
+            'created_at' => $this->created_at?->format('Y-m-d H:i:s'),
+            'updated_at' => $this->updated_at?->tolso8601String(),
+        ];
     }
 }
